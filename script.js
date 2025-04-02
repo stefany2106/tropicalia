1ba0100bc459891d24b65c130a6a0cb8d397fb96
stefany2106 <silva.pereira.stefany@escola.pr.gov.br>
2025-04-02 12:22:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
+    const alternaContraste = document.getElementById('alterna-contraste');
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
@@ -24,4 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+})
 })
